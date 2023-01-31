1a47a893658104f34f90bf2936fa0bb12623a888
MishaPvl <godslayerepta@gmail.com>
2023-01-31 21:18:01 +0700

    Добавлено приложение для списков

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -10,15 +10,19 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         '''установка'''
-        s = Service('\.venv\Scripts\geckodriver.exe')
-        self.browser = webdriver.Firefox(service=s)
+        self.s = Service('\.venv\Scripts\geckodriver.exe')
+        self.browser = webdriver.Firefox(service=self.s)
+
+    def tearDown(self) -> None:
+        self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-    # В браузере открывется страница сайта
+        # В браузере открывется страница сайта
         self.browser.get('http://localhost:8000')
 
-    # В заголовке сайта написано 'To-Do'
-    assert 'To-Do' in browser.title
+        # В заголовке сайта написано 'To-Do'
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест')
 
 # Предлагается ввести элемент списка
 
@@ -32,4 +36,5 @@ class NewVisitorTest(unittest.TestCase):
 
 # Сайт генерирует уникальный URl для каждого пользователя 
 
-browser.quit()
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')

b51fcf19070bf62c6883c8a251917637e9a6e334
MishaPvl <godslayerepta@gmail.com>
2023-01-31 22:49:47 +0700

    Заглушка для теста

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    '''Тест на токсичность'''
+class HomePageTest(TestCase):
+    '''Тест домашней страницы'''
 
-    def test_bad_maths(self):
-        '''тест: неправильные математические расчеты'''
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        '''тест: корневой url преобразуется в представление домашней страницы'''
+        found = resolve('/')
+        # Мы проверяем, чтобы resolve, когда ее вызывают с «/», то есть корнем сайта, 
+        # нашла функцию под названием home_page
+        self.assertEqual(found.func, home_page)
